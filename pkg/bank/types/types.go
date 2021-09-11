c53a755a30d8dbf11a99d84ffd5a40134d963860
rustam fozilov <rustamfozilov1998@gmail.com>
2021-09-11 18:37:55 +0300

    Добавлены типы

---
+++ pkg/bank/types/types.go
@@ -0,0 +1,35 @@
+package types
+
+// Amount of money in smallest units (cent, diram etc.)
+type Money int64
+
+// Represent the code of currency
+type Currency string
+
+// Codes of currency
+const (
+	TJS Currency = "TJS"
+	USD Currency = "USD"
+	RUB Currency = "RUB"
+)
+
+// Number of card
+type PAN string
+
+// Card represents information of card
+type Card struct {
+	ID       int
+	PAN      PAN
+	Balance  Money
+	Currency Currency
+	Color   string
+	Name     string
+	Active bool
+	MinBalance Money
+}
+
+//Payment represents information about payments, but this comment is redundant
+type Payment struct {
+	ID int
+	Amount Money
+}
